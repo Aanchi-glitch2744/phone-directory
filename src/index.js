d8b604657422cb5f12839077daa6c0230d575fff
Aanchi-glitch2744 <63657840+Aanchi-glitch2744@users.noreply.github.com>
2021-07-04 11:02:39 +0530

    Added 'AddSubscriber' component

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddSusbscriber from './AddSubscriber';
+import './common/common.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddSusbscriber />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
